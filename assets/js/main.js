52c89f30023b39a106378fbbe2834115f2f2abfe
Igo RPL <igorpl1289845@gmail.com>
2023-05-14 09:56:44 +0700

    Project 1, Membuat Website Sederhana HTML CSS JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,25 @@ window.addEventListener("scroll", function () {
   var navSticky = document.querySelector(".navbar");
   navSticky.classList.toggle("sticky", this.window.scrollY > 0);
 });
+
+// scroll delay
+const sr = ScrollReveal({
+  distance: "30px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(" .content-home", {delay: 400, origin: "left"});
+sr.reveal(" .heading", {delay: 100, origin: "left"});
+sr.reveal(" .social-media", {delay: 50, origin: "bottom"});
+sr.reveal(" .about-row", {delay: 50, origin: "top"});
+sr.reveal(" .box1", {delay: 100, origin: "top"});
+sr.reveal(" .box2", {delay: 500, origin: "top"});
+sr.reveal(" .box3", {delay: 1000, origin: "top"});
+sr.reveal(" .box4", {delay: 100, origin: "top"});
+sr.reveal(" .box5", {delay: 500, origin: "top"});
+sr.reveal(" .box6", {delay: 1000, origin: "top"});
+sr.reveal(" .box7", {delay: 100, origin: "top"});
+sr.reveal(" .box8", {delay: 500, origin: "top"});
+sr.reveal(" .box9", {delay: 1000, origin: "top"});
+sr.reveal(" .contact-row", {delay: 50, origin: "top"});
